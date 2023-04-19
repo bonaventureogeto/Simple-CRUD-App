993dddf8479b1df4bfa8146f10846a8898013bfb
Bonaventure <bonaogeto1@gmail.com>
2023-04-19 19:33:08 +0300

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,67 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import axios from "axios";
+import React, { useEffect, useState } from "react";
 
-function App() {
+const baseURL = "https://jsonplaceholder.typicode.com/posts";
+
+export default function App() {
+  const [post, setPost] = useState(null);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    axios
+      .get(`${baseURL}/1`)
+      .then((response) => {
+        setPost(response.data);
+      })
+      .catch((error) => {
+        setError(error.message);
+      });
+  }, []);
+
+  function createPost() {
+    axios
+      .post(baseURL, {
+        title: "Hello World!",
+        body: "This is a new post.",
+      })
+      .then((response) => {
+        setPost(response.data);
+      });
+  }
+
+  function updatePost() {
+    axios
+      .put(`${baseURL}/1`, {
+        title: "Updated Hello World!",
+        body: "This is an updated post.",
+      })
+      .then((response) => {
+        setPost(response.data);
+      });
+  }
+
+  function deletePost() {
+    axios.delete(`${baseURL}/1`).then(() => {
+      alert("Are you sure?");
+      setPost(null);
+    });
+  }
+
+  // if (!post) return "DELETED!!!!";
+  if (error) return `Error: ${error.message}`;
+  if (!post) return "Loading...";
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>The Galaxy News</h1>
+      <div>
+        <h1>{post.title}</h1>
+        <p>{post.body}</p>
+        <button onClick={createPost}>Create Post</button>
+        <button onClick={updatePost}>Update Post</button>
+        <button onClick={deletePost}>Delete Post</button>
+      </div>
     </div>
   );
 }
-
-export default App;
